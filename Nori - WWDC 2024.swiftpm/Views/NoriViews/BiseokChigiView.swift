44604834027053da1a3ce6aebeef81f1187698d8
squattlife <migonyoung01@gmail.com>
2024-01-28 14:15:18 +0900

    SplitView

---
+++ Nori - WWDC 2024.swiftpm/Views/NoriViews/BiseokChigiView.swift
@@ -0,0 +1,50 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 이재영 on 1/26/24.
+//
+
+import SwiftUI
+
+struct BiseokChigiView: View {
+    var body: some View {
+        splitView
+    }
+    
+    // MARK: 뷰 두개로 나누기
+    var splitView: some View {
+        HStack(spacing: 0) {
+            leftView
+                .frame(maxWidth: .infinity)
+
+            Divider()
+                .padding()
+                
+            rightView
+                .frame(maxWidth: .infinity)
+        }
+    }
+    
+    var leftView: some View {
+        VStack {
+            Text("Left View")
+                .font(.largeTitle)
+                .padding()
+            // Add your left view content here
+        }
+    }
+    
+    var rightView: some View {
+        VStack {
+            Text("Right View")
+                .font(.largeTitle)
+                .padding()
+            // Add your right view content here
+        }
+    }
+}
+
+#Preview {
+    BiseokChigiView()
+}
